34a32e874835ec3140019ce4b69e10b4048bf3e3
antony <antony@localhost.org>
2014-07-01 22:41:12 +0300

    changes to java files

--- java c-s code/Client.java
+++ java c-s code/Client.java
@@ -1,66 +1,225 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.net.InetAddress;
-import java.net.Socket;
 
-import javax.swing.JOptionPane;
- 
-public class Client 
-{
- 
-    private static Socket socket;
- 
-    public static void main(String args[])
-    {
-        try
-        {
-            String host = "localhost";
-            int port = 25002;
-            InetAddress address = InetAddress.getByName(host);
-            socket = new Socket(address, port);
- 
-            //Send the message to the server
-            OutputStream os = socket.getOutputStream();
-            OutputStreamWriter osw = new OutputStreamWriter(os);
-            BufferedWriter bw = new BufferedWriter(osw);
- 
-            String number = null;
-			try {
-				number = JOptionPane.showInputDialog(null, "type something...");
-			} catch (NullPointerException e) {
-				System.out.println("no empty text allowed");
-			}
- 
-            String sendMessage = number + "\n";
-            bw.write(sendMessage);
-            bw.flush();
-            System.out.println("Message sent to the server : "+sendMessage);
- 
-            //Get the return message from the server
-            InputStream is = socket.getInputStream();
-            InputStreamReader isr = new InputStreamReader(is);
-            BufferedReader br = new BufferedReader(isr);
-            String message = br.readLine();
-            System.out.println("Message received from the server : " +message);
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+public class Client {
+
+    // the client needs a GUI so;
+    private chatClient c;
+    private String server, username;
+    private int port;
+    private ObjectInputStream server_input; // to read from the socket
+    private ObjectOutputStream server_output; // to write on the socket
+    private Socket sock;
+
+    Client(String server, int port, String username) {
+        // for just a console session, then set the gui object to null
+        this(server, port, username, null);
+    }
+
+    Client(String server, int port, String username, chatClient c) {
+        super();
+        this.c = c;
+        this.server = server;
+        this.username = username;
+        this.port = port;
+    }
+
+    // chat now
+    public boolean begin_chat() {
+        // try to connect to the server
+        try {
+            sock = new Socket(server, port);
+        } catch (IOException e) {
+            display_client_msg("Could not connect to the server.\nCheck your hostname/port combination and try again: "
+                    + e);
+            return false;
         }
-        catch (Exception exception) 
-        {
-            exception.printStackTrace();
+        String msg = "The connection to " + sock.getInetAddress() + ":"
+                + sock.getPort() + " was successful";
+        display_client_msg(msg);
+
+        /* create I/O streams */
+        try {
+            server_input = new ObjectInputStream(sock.getInputStream());
+            server_output = new ObjectOutputStream(sock.getOutputStream());
+        } catch (IOException ex) {
+            display_client_msg("Encountered an I/O error: " + ex);
+            return false;
         }
-        finally
-        {
-            //Closing the socket
-            try
-            {
-                socket.close();
+
+        // creates the Thread to listen from the server
+        new msg_from_server().start();
+        try {
+            server_output.writeObject(username); // send username to server
+        } catch (IOException e) {
+            display_client_msg("Encountered an error when trying to log you in : "
+                    + e);
+            disconnect(); // let them retry
+            return false;
+        }
+        return true;
+    }
+
+    // save many println's
+    private void display_client_msg(String msg) {
+        if (c == null) {
+            System.out.println(msg);
+        } else {
+            c.append(msg + "\n");
+        }
+    }
+
+    // when a disonnnect is issued
+    private void disconnect() {
+
+        if (server_input != null) {
+            try {
+                server_input.close();
+            } catch (IOException e1) {
+                display_client_msg("" + e1); //really this isn't required but nway t seems handy 4 debugging;
             }
-            catch(Exception e)
-            {
-                e.printStackTrace();
+        }
+
+        if (server_output != null) {
+            try {
+                server_output.close();
+            } catch (IOException e) {
+                display_client_msg("" + e);
+            }
+        }
+
+        if (sock != null) {
+            try {
+                sock.close();
+            } catch (IOException e) {
+                display_client_msg("" + e);
+            }
+        }
+
+        if (c != null) {
+            c.connection_error();
+        }
+
+    }
+
+    /*
+     * To send a message to the server
+     */
+    void send_msg_to_server(chat_msg_type msg) {
+        try {
+            server_output.writeObject(msg);
+        } catch (IOException e) {
+            display_client_msg("Encountered an error sending message to the server: "
+                    + e);
+        }
+    }
+
+    public static void main(String[] args) {
+        int server_port_no = 25500;
+        String usr_name = "Unknown";
+        Scanner s = new Scanner(System.in);
+        String server_ip = null;
+
+        try {
+            System.out.println("Enter the hostname/ip for the server\n");
+            server_ip = s.nextLine();
+            System.out.println("Enter the server's listening port\n");
+            server_port_no = s.nextInt();
+        } catch (Exception e1) {
+            System.out.println("Error " + e1);
+            System.exit(1);
+        }
+
+        s.close();
+        // cheking args
+        switch (args.length) {
+            case 3:
+                server_ip = args[2];
+            case 2:
+                try {
+                    server_port_no = Integer.parseInt(args[1]);
+                } catch (NumberFormatException e) {
+                    System.out.println("You entered an invalid port number.");
+                    return;
+                }
+            case 1:
+                usr_name = args[0];
+            case 0:
+                break;
+            // invalid number of arguments
+            default:
+                return;
+        }
+        // create the Client object
+        Client client = new Client(server_ip, server_port_no, usr_name);
+        if (!client.begin_chat()) // if we can't connect
+        {
+            return;
+        }
+
+        // wait for messages from user
+        Scanner scan = new Scanner(System.in);
+        // loop and wait for message from the user
+        while (true) {
+            System.out.print("# ");
+            // read message from user and break on error
+            String msg = null;
+            try {
+                msg = scan.nextLine();
+            } catch (Exception e) {
+                break;
+            }
+            // logic of course
+            if (msg.equals("EXIT")) {
+                client.send_msg_to_server(new chat_msg_type(chat_msg_type.BYE,
+                        ""));
+                // disconnect
+                break;
+            } // get logged in users
+            else if (msg.equals("WHO")) {
+                client.send_msg_to_server(new chat_msg_type(chat_msg_type.WHO,
+                        ""));
+            } else { // default to ordinary message
+                client.send_msg_to_server(new chat_msg_type(
+                        chat_msg_type.CHATMSG, msg));
+            }
+        }
+        // done disconnect
+        scan.close();
+        client.disconnect();
+    }
+
+    // append or println whichever comes
+    class msg_from_server extends Thread {
+
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    String msg = (String) server_input.readObject();
+                    // if console mode print the message and add back the prompt
+                    if (c == null) {
+                        System.out.println(msg);
+                        System.out.print("# ");
+                    } else {
+                        c.append(msg);
+                    }
+                } catch (IOException e) {
+                    display_client_msg("Your connection has been closed by the server");
+                    // if there's a gui then call the gui function for a failed
+                    // connection
+                    if (c != null) {
+                        c.connection_error();
+                    }
+                    break;
+                } // this gets caught in a telnet but i actually don't know why
+                // i think its coz a string wasn't captured from the connection
+                // stream
+                catch (ClassNotFoundException e2) {
+                    System.exit(1);
+                }
             }
         }
     }
--- java c-s code/Server.java
+++ java c-s code/Server.java
@@ -1,73 +1,394 @@
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
+import java.awt.HeadlessException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Vector;
 
 import javax.swing.JOptionPane;
- 
-public class Server 
-{
- 
-    private static Socket socket;
- 
-    public static void main(String[] args) 
-    {
-        try
-        {
- 
-            int port = 25002;
-            ServerSocket serverSocket = new ServerSocket(port);
-            System.out.println("Server Started and listening to the port "+ port);
- 
-            //Server is running always. This is done using this while(true) loop
-            while(true) 
-            {
-                //Reading the message from the client
-                socket = serverSocket.accept();
-                InputStream is = socket.getInputStream();
-                InputStreamReader isr = new InputStreamReader(is);
-                BufferedReader br = new BufferedReader(isr);
-                String number = br.readLine();
-                System.out.println("Message received from client is "+number);
- 
-                
-                String returnMessage = null;
-                try
-                {
-                	returnMessage = JOptionPane.showInputDialog(null, "enter a message to the client");
-                }
-                catch(NullPointerException e)
-                {
-                    //disallow blanks
-                	JOptionPane.showInputDialog(null, "no empty text allowed");
-                }
- 
-                //Sending the response back to the client.
-                
-                OutputStream os = socket.getOutputStream();
-                OutputStreamWriter osw = new OutputStreamWriter(os);
-                BufferedWriter bw = new BufferedWriter(osw);
-                bw.write(returnMessage);
-                System.out.println("Message sent to the client is "+returnMessage);
-                bw.flush();
-            }
+
+public class Server {
+
+    private int port_number;
+    private InetAddress addr;
+    private static int client_id; // track connections to the server
+    // is server running or not ; true/false respectively
+    private boolean is_running;
+    // vector for storage of connected clients
+    private Vector<thread_of_client> client;
+    @SuppressWarnings("unused")
+    private Object g = null;
+    // this format will do us good
+    private SimpleDateFormat dt = new SimpleDateFormat("HH:mm:ss");
+    private Socket client_sock; // client
+    private ServerSocket sock; // server
+
+    // private static int timeout = 10;
+    public Server(int port) {
+        this(port, null);
+    }
+
+    // if we ever need a gui for the server, then this would come in handy
+    public Server(int port, Object gui) {
+        this.g = gui;
+        this.port_number = port;
+        // initialize the vector array
+        client = new Vector<>();
+    }
+
+    // starts the server
+    public void start() {
+        is_running = true;
+        /*
+         * let user define the socket and hostname used by the server exit on
+         * error
+         */
+        String ip = null;
+        try {
+            ip = JOptionPane
+                    .showInputDialog(
+                            null,
+                            "Now enter the hostname/ip for the listening interface\n"
+                            + "e.g 127.0.0.1, localhost, mydomain.com, or even your computers name",
+                            "127.0.0.1");
+        } catch (Exception e1) {
+            print_msg("Try again coz the server cant proceed. Error is " + e1);
+            System.exit(1);
         }
-        catch (Exception e) 
-        {
-            e.getMessage();
+
+        sock = null;
+        // try catch to check for invalid hostnames,etc
+        try {
+            // sock.setSoTimeout(timeout);
+            addr = InetAddress.getByName(ip);
+            sock = new ServerSocket(port_number, 20, addr);
+        } catch (IOException e1) {
+            print_msg("Try again because the server can't proceed. Error is "
+                    + e1);
+            System.exit(1);
         }
-        finally
-        {
-            try
-            {
-                socket.close();
+        // if successful which is the case if
+        // the code reaches here, then display this on the console
+        JOptionPane.showMessageDialog(null,
+                "Now, take the port & hostname you typed initially\n"
+                + "and use em on a client "
+                + "to initialize a connection", "Success",
+                JOptionPane.INFORMATION_MESSAGE);
+        print_msg("the server is listening for connections on "
+                + sock.getInetAddress() + ":" + sock.getLocalPort());
+
+        // infinite loop to wait for connections
+        while (is_running) {
+            // accept client connection
+            client_sock = null;
+            try {
+                client_sock = sock.accept();
+            } catch (IOException e) {
+                print_msg("Your connection was't accepted by server. Error is "
+                        + e);
             }
-            catch(Exception e){}
+            // get out of loop if needed
+            if (!is_running) {
+                break;
+            }
+
+            // otherwise create a thread instance of the client connection
+            thread_of_client t = new thread_of_client(client_sock);
+            client.add(t); // and save it in the vector array
+            // use
+            t.start(); // then start the thread
+        }
+        // come here when server is supposed to stop
+        try {
+            sock.close();
+            // close client connections
+            for (int i = 0; i < client.size(); ++i) {
+                thread_of_client tc = client.get(i);
+                try {
+                    tc.server_input.close();
+                    tc.server_output.close();
+                    tc.socket.close();
+                } catch (IOException x) {
+                    print_msg("Could not close all client connections. Error is "
+                            + x);
+                }
+            }
+        } catch (IOException e) {
+            print_msg("Encounterd an error when closing the server and disconnecting clients: "
+                    + e);
         }
     }
+
+    // saves the task of having to system.out all the time
+    private void print_msg(String msg) {
+        String time_and_msg = dt.format(new Date()) + " " + msg;
+        System.out.println(time_and_msg);
+    }
+
+    /*
+     * Broadcasts section **** Broadcasts simply travel to all clients connected
+     * an example would be a sever shutdown or error in this case, ours can be
+     * the chat messages also and they are synchronized across the thread
+     * instances
+     */
+    private synchronized void broadcast(String brodcast_msg) {
+        String time = dt.format(new Date());
+        System.out.print("broadcast at " + time + " >" + brodcast_msg + "\n");
+        // reverse loop for checking for unavailable clients
+        for (int i = client.size(); --i >= 0;) {
+            thread_of_client c = client.get(i);
+            // if a broadcast can't pass through to a connected
+            // client then remove it from the list
+            if (!c.chat(time + " " + brodcast_msg + "\n")) {
+                client.remove(i);
+                print_msg("client " + c.client_usrname
+                        + " has disconnected is no longer available.");
+            }
+        }
+    }
+
+    // remove a user from the vector array who exits e.g by closing the chat
+    // window or logging out
+    synchronized void remove(int id) {
+        for (int i = 0; i < client.size(); ++i) {
+            thread_of_client ct = client.get(i);
+            if (ct.id == id) {
+                client.remove(i);
+                return;
+            }
+        }
+    }
+
+    // stop server
+    protected void stop() {
+        is_running = false;
+        try {
+        	 // tried a broadcast to all users to inform them of a shutdown, but makes perfect
+        	// sense if the server had a GUI
+        	broadcast("The server has been stopped");
+        	sock.close();
+        } catch (IOException e) {
+            print_msg("Encountred an error while shutting down the server: "
+                    + e);
+        }
+    }
+
+    /* the main aka entry point for the program is here */
+    public static void main(String[] args) {
+        // start server on port 25500 unless specified
+        int port_number = 25500;
+        // nimbus look and feel
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager
+                    .getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Server.class.getName()).log(
+                    java.util.logging.Level.SEVERE, null, ex);
+        }
+        try {
+            port_number = Integer.parseInt(JOptionPane.showInputDialog(null,
+                    "specify a port for the server to listen on.\n"
+                    + "e.g 25500 or proceed with 0 which \n "
+                    + "generates a random port", "0"));
+        } catch (NumberFormatException | HeadlessException e1) {
+            System.out.println(e1);
+            System.exit(1);
+        }
+        // this would help in a console session where args would be needed or
+        // for a telnet connection which refuses to work in this case
+        switch (args.length) {
+            // 1 arg specified
+            case 1:
+                try { // check if arg supplied is a port (integer)
+                    port_number = Integer.parseInt(args[0]);
+                } catch (NumberFormatException e) {
+                    System.out.println("an invalid port number was specified.");
+                    System.out
+                            .println("goto the compiled java classes directory\nthen type java Server 25500");
+                    System.exit(1);
+                }
+            case 0:
+                break;
+            default:
+                System.out
+                        .println("goto the compiled java classes directory\nthen type java Server 25500");
+                System.exit(1);
+
+        }
+        // a valid start
+        Server s = new Server(port_number);
+        s.start();
+    }
+
+    /*
+     * program has one thread but an instance of this thread will run for each
+     * client who connects
+     */
+    class thread_of_client extends Thread {
+
+        // the socket where to listen/talk
+        Socket socket;
+        ObjectInputStream server_input;
+        ObjectOutputStream server_output;
+        int id;
+        String client_usrname;
+        chat_msg_type cm; // chat object message
+        String date;
+
+        thread_of_client(Socket socket) {
+            // increment dis to mek t uniq e.g in the instance where two users
+            // have similar usernames
+            id = ++client_id;
+            this.socket = socket;
+            print_msg("I/O is taking place");
+            try {
+                // create output first
+                server_output = new ObjectOutputStream(socket.getOutputStream());
+                server_input = new ObjectInputStream(socket.getInputStream());
+                // username
+                client_usrname = (String) server_input.readObject();
+                // print_msg(client_usrname + " has just connected.");
+                broadcast(client_usrname + " has has logged in from "
+                        + client_sock.getInetAddress());
+            } catch (IOException e) {
+                print_msg("Server encounered an error sending data: " + e);
+                return;
+            } catch (ClassNotFoundException e) {
+            }
+            date = new Date().toString() + "\n";
+        }
+
+        // send chat messages to client
+        private boolean chat(String msg) {
+            // this is only possible if they ar connected
+            if (!socket.isConnected()) {
+                close();
+                return false;
+            }
+            // if they are available, write msg to output stream
+            try {
+                server_output.writeObject(msg);
+            } catch (IOException e) {
+                // print the error to the console
+                print_msg(client_usrname + " : Error sending message "
+                        + e.toString());
+            }
+            return true;
+        }
+
+        // runs until user logoff
+        @Override
+        public void run() {
+            // to loop until EXIT
+            boolean server_running = true;
+            while (server_running) {
+                // read a String (which is an object)
+                try {
+                    cm = (chat_msg_type) server_input.readObject();
+                } catch (IOException e) {
+                    print_msg(client_usrname
+                            + " has encountered an I/O error. Looks like its a sudden reset"
+                            + e);
+                    broadcast(client_usrname + " has disconnected");
+                    break;
+                } catch (ClassNotFoundException e2) { //just catch it!
+                    break;
+                }
+                // chat
+                String message = cm.getMessage();
+
+                // capture a variety of this chat messages
+                switch (cm.getType()) {
+
+                    case chat_msg_type.CHATMSG:
+                        // send a chat everywhere
+                        broadcast(client_usrname + ": " + message);
+                        break;
+                    case chat_msg_type.BYE:
+                        // print_msg(client_usrname + " has logged off");
+                        broadcast(client_usrname + " has logged off at "
+                                + dt.format(new Date()));
+                        server_running = false;
+                        break;
+                    case chat_msg_type.WHO:
+                        chat("users connected at " + dt.format(new Date()) + "\n");
+                        // scan all the users connected
+                        for (int i = 0; i < client.size(); ++i) {
+                            thread_of_client ct = client.get(i);
+                            chat((i + 1) + ") " + ct.client_usrname + " since "
+                                    + ct.date);
+                        }
+                        break;
+                }
+            }
+            // we don't need the host being shown among the connected clients
+            remove(id);
+            close();
+        }
+
+        // try to close everything
+        private void close() {
+            // try to close the connection
+            try {
+                if (server_output != null) {
+                    server_output.close();
+                }
+            } catch (IOException e) {
+            }
+            try {
+                if (server_input != null) {
+                    server_input.close();
+                }
+            } catch (IOException e) {
+            }
+            try {
+                if (socket != null) {
+                    socket.close();
+                }
+            } catch (IOException e) {
+            }
+        }
+
+    }
+}
+
+// WHO shows connected users when invoked by client/server
+// message is a chat message
+// EXIT logs a user who invokes it out of the system
+class chat_msg_type implements Serializable {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = -3875380843739221253L;
+    static final int WHO = 0;
+    static final int CHATMSG = 1;
+    static final int BYE = 2;
+    private final int type;
+    private final String message;
+
+    chat_msg_type(int type, String message) {
+        this.type = type;
+        this.message = message;
+    }
+
+    int getType() {
+        return type;
+    }
+
+    String getMessage() {
+        return message;
+    }
+
 }
